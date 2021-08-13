b71be4cdd124e72ff82aeef73696d273794eb53a
Karim Hesham <karimhesham00@hotmail.com>
2021-08-13 17:25:19 +0200

    Commit Files

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap -y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>New rooms</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Deluxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is a clone</p>
+        <p>Referrals accepted</p>
+        <p>Karim Hesham</p>
+        <p>React Js</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Karim Hesham</p>
+        <p>Frontend developer</p>
+        <p>Airbnb Clone</p>
+        <p>Web Development</p>
+        <p>Join Now</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Contact Us</p>
+        <p>Careers</p>
+        <p>Our Team</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
