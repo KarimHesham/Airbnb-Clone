3fe6fbb930696ee2b7a36e353bf2b3dc329337f6
Karim Hesham <karimhesham00@hotmail.com>
2021-08-14 15:23:28 +0200

    MapBox Integration - Map Marker - Map PopUps

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoia2FyaW1oZXNoYW0iLCJhIjoiY2tzYnE0dzZtMDk2cDJ2bnVlMGN3d3h0ZCJ9.ozDtP7WEpLMfOl1j_MwnYw",
+  },
 };
